87a6c5b5af6b0ab0023cebe554f78a65630bb63c
kmassey-TUS <98782937+kmassey-TUS@users.noreply.github.com>
2022-02-22 00:15:38 +0000

    Adding backup repository of code

---
+++ Games/src/main/java/com/tus/games/GamesSpringApp.java
@@ -0,0 +1,15 @@
+package com.tus.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GamesSpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesSpringApp.class, args);
+
+	}
+
+}
---
+++ Games/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO game_detail (id, genre, price) VALUES (1, 'Action', 21), (2, 'Horror', 32), (3, 'Puzzle', 50) ;
+INSERT INTO games (id, console, game_name, user_detail_id) VALUES (1, 'Playstation 4', 'COD 3', 1);
+INSERT INTO games (id, console, game_name, user_detail_id) VALUES (2, 'XBOX 360', 'F.E.A.R 2', 2);
+INSERT INTO games (id, console, game_name, user_detail_id) VALUES (3, 'WII U', 'House', 3);
+INSERT INTO games (id, console, game_name, user_detail_id) VALUES (7, 'PS5', 'Horizon Zero Dawn');
+INSERT INTO games (id, console, game_name, user_detail_id) VALUES (4, 'XBOX X', 'God of War');
